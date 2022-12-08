93b7aaaef3479902c76711ceb6bd885e0b932a9e
Grzegorz Opara <grzegorz.opara@gmail.com>
2022-12-08 22:05:58 +0100

    removed desc from varibales

--- variables.tf
+++ variables.tf
@@ -1,23 +1,19 @@
 variable "ARM_CLIENT_ID" {
-  description = "Azure SP name"
   type        = string
   sensitive   = true
 }
 
 variable "ARM_CLIENT_SECRET" {
-  description = "SP Secret"
   type        = string
   sensitive   = true
 }
 
 variable "ARM_SUBSCRIPTION_ID" {
-  description = "Azure Subscription ID"
   type        = string
   sensitive   = true
 }
 
 variable "ARM_TENANT_ID" {
-  description = "Azure Tenant ID"
   type        = string
   sensitive   = true
 }
