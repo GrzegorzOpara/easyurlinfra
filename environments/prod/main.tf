df0664e6bbb615d34501020acfbfa1f70ad0f9ae
Grzegorz Opara <gopara@coldfusion.onmicrosoft.com>
2022-12-10 17:46:46 +0100

    specified python version

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_linux_web_app" "webapp" {
   site_config { 
     minimum_tls_version = "1.2"
       application_stack {
-        current_stack  = "PYTHON:3.9"                                      
+        python_version = "3.9"
       }
   }
 }
